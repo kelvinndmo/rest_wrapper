1b8d0fe9ce9604af5db10ec26b562a69a9ce71bd
shemogumbe <shem.ogumbe@andela.com>
2019-08-17 07:56:40 +0300

    added query for posts from api

--- api/schema.py
+++ api/schema.py
@@ -2,8 +2,14 @@ import json
 from graphene import ObjectType, String, Boolean, ID, Field, List
 from graphene_sqlalchemy import SQLAlchemyObjectType
 
-from .utils import get_users_from_app
+from .utils import get_users_from_app, get_posts_from_app
 from .models import User as UserModel, Post as PostModel, Comment as CommentModel
+
+
+class Post(ObjectType):
+    post = String()
+    image = String()
+    user_id = ID()
 
 class User(ObjectType):
     first_name = String()
@@ -11,10 +17,6 @@ class User(ObjectType):
     active = Boolean()
     avatar = String()
 
-
-class Post(SQLAlchemyObjectType):
-    class Meta:
-        model = PostModel
 
 
 class Comment(SQLAlchemyObjectType):
@@ -32,12 +34,14 @@ class Query(ObjectType):
         users = res["users"]
         print(users)
         users_obj_list = [User(**user) for user in users]
-        # for user in users:
-        #     user_obj = User(**user)
-        #     print('Singe', user_obj)
-        #     users_obj_list.append(user_obj)
         return users_obj_list
 
-    def resolve_posts(self, info , **kwargs):
-        posts = Post.get_query(info)
-        return posts.all()
+    def resolve_posts(self, info):
+        res = get_posts_from_app()
+        posts = res["posts"]
+        post_obj_list = []
+        for post in posts:
+            post.update({'user_id': post.pop('user')})
+            post_obj = Post(**post)
+            post_obj_list.append(post_obj)
+        return post_obj_list
--- api/utils.py
+++ api/utils.py
@@ -8,3 +8,8 @@ def get_users_from_app():
     users = res.content
     return json.loads(users)
 
+
+def get_posts_from_app():
+    res = requests.get('http://127.0.0.1:5000/posts')
+    posts = res.content
+    return json.loads(posts)
